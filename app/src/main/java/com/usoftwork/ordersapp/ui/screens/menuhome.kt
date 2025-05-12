a054e3822bae569b7d6f585edeb406b3380d315f
sebas73 <sandresesquivel04@gmail.com>
2025-05-12 19:38:10 -0400

    V0.0.4.5 CHANGES NOTES: THE NEXT SLIDES WE CAN WORK WITH LANDSCAPE FORMAT:
    - LOGIN
    - REGISTER
    - MENU/HOME
    
    NEW SUB-SLIDE TO CONTRACT MODES.
    
    FUTURE CHANGES TO V0.0.5:
    - MAKE A MODE RECOVERY PASSWORD FUNCTIONALLY
    - CONNECT APP WHIT THE SQL SERVER AND GOOGLE CLOUD API
    - MAKES SLIDES LIKE: CREATE PRODUCT/ ANALYSIS AND STATISTICS/ ADMIN ADMINISTRATIONS

--- app/src/main/java/com/usoftwork/ordersapp/ui/screens/menuhome.kt
+++ app/src/main/java/com/usoftwork/ordersapp/ui/screens/menuhome.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.List
