93893109c5eeca3314743ce33ff24b67c80cc149
JuliOOX <158847614+JuliOOX@users.noreply.github.com>
2025-06-16 19:40:28 -0400

    Add files via upload

---
+++ _Almacen/a1Componente.kt
@@ -0,0 +1,17 @@
+//package com.example.dblocal._Almacen
+
+package com.example.db_local_menu._Almacen
+
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "Componente")
+data class a1Componente (
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val nombre: String,
+    val porcion: Double,
+    val unidad: String,
+    val cantidad: Int
+)
