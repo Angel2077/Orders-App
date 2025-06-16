bd43646be33c4870226c4dc4b3b3f6bda704e7aa
angel2077 <angelomoralesibanez@gmail.com>
2025-06-16 19:32:34 -0400

    Limpieza de Branch

--- dbLocal/app/src/main/java/com/example/dblocal/_Ejemplo/a3EntityDataBase.kt
+++
@@ -1,9 +0,0 @@
-package com.example.dblocal._Ejemplo
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = [a1Entity::class], version = 1)
-abstract class AppDatabase : RoomDatabase() {
-    abstract fun EntityDao(): a2EntityDao
-}
--- dbLocal/app/src/main/java/com/example/dblocal/_Ejemplo/a4Repository.kt
+++
@@ -1,6 +0,0 @@
-package com.example.dblocal._Ejemplo
-
-class a4Repository(private val dao: a2EntityDao) {
-    suspend fun insertar(entity: a1Entity) = dao.insertar(entity)
-    fun obtener() = dao.obtener()
-}
